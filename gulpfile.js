57bc30d0f2200c38881ad7393ea3796f39f9b431
Chris Van Patten <chris@vanpattenmedia.com>
2016-11-02 11:43:51 -0400

    updates to vpmframe-static

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,105 @@
-var gulp  = require('gulp');
-var gutil = require('gulp-util');
+/**
+ * Load Gulp and Gulp-adjacent dependencies
+ */
+var gulp           = require('gulp'),
+    gutil          = require('gulp-util'),
+    concat         = require('gulp-concat'),
+    cssnano        = require('gulp-cssnano'),
+    imagemin       = require('gulp-imagemin'),
+    livereload     = require('gulp-livereload'),
+    mainBowerFiles = require('main-bower-files'),
+    plumber        = require('gulp-plumber'),
+    sass           = require('gulp-sass'),
+    sassAssetFuncs = require('node-sass-asset-functions'),
+    sassglob       = require('gulp-sass-glob'),
+    ;
 
-var concat     = require('gulp-concat');
-var uglify     = require('gulp-uglify');
-var compass    = require('gulp-compass');
-var imagemin   = require('gulp-imagemin');
-var minifyCSS  = require('gulp-minify-css');
-var livereload = require('gulp-livereload');
-
-var dev = 'app/assets/';
-var pub = 'public/assets/';
+/**
+ * Define paths
+ */
+var src  = 'resources/assets/',
+    dest = 'public/assets/'
+    ;
 
 var paths = {
-	dev: {
-		fonts:       dev + 'fonts/**',
-		images:      dev + 'images/*',
-		javascripts: dev + 'javascripts/*',
-		sass:        dev + 'sass',
-		sass_all:   [dev + 'sass/*', dev + 'sass/**']
-	},
-	pub: {
-		css:         pub + 'css/',
-		fonts:       pub + 'fonts/',
-		images:      pub + 'img/',
-		javascripts: pub + 'js/'
-	}
+  src: {
+    sass:   src + 'sass'
+    fonts:  src + 'fonts/**/*',
+    images: src + 'images/**/*',
+  },
+  dest: {
+    css:    dest + 'css/',
+    fonts:  dest + 'fonts/',
+    images: dest + 'img/',
+  }
 };
 
+/**
+ * CSS tasks
+ */
 gulp.task('css', function() {
-	gulp.src(paths.dev.sass + '/*.scss')
-		.pipe(compass({
-			sass:       paths.dev.sass,
-			css:        paths.pub.css,
-			image:      paths.pub.images,
-			font:       paths.pub.fonts,
-			require: [
-				'susy',
-				'rgbapng',
-				'breakpoint',
-				'sass-globbing',
-				'compass-normalize'
-			],
-			bundle_exec: true
-		}))
-		.on('error', gutil.log)
-		.pipe(minifyCSS())
-		.pipe(gulp.dest(paths.pub.css));
+  gulp.src(paths.src.sass + '/*.scss')
+    .pipe(sassglob())
+    .pipe(sass({
+      functions: sassAssetFuncs({
+        'images_path': paths.dest.images,
+        'http_images_path': '/assets/img/'
+      }),
+      includePaths: [
+        './vendor/bower_components',
+        './vendor/bower_components/breakpoint-sass/stylesheets'
+      ]
+    }).on('error', sass.logError))
+    .pipe(cssnano({
+      autoprefixer: {
+        browsers: ['last 2 versions'],
+        cascade: false
+      },
+      discardComments: {
+        removeAll: true
+      }
+    }))
+    .pipe(gulp.dest(paths.dest.css))
+    .pipe(livereload());
 });
 
-gulp.task('images', function () {
-	gulp.src(paths.dev.images)
-		.pipe(imagemin())
-		.pipe(gulp.dest(paths.pub.images));
+/**
+ * Images
+ */
+gulp.task( 'images', function () {
+  gulp.src(paths.src.images)
+    .pipe(imagemin({
+      progressive: true,
+      multipass: true
+    }))
+    .pipe(gulp.dest(paths.dest.images))
+    .pipe(livereload());
 });
 
-gulp.task('watch', function() {
-	gulp.watch(paths.dev.sass_all, ['css']);
-	gulp.watch(paths.dev.images, ['images']);
+/**
+ * Watch for changes and automatically reload the browser
+ */
+gulp.task('watcher', function() {
+  // Activate LiveReload's listener
+  livereload.listen();
 
-	gulp.watch(pub + '**').on('change', function(file) {
-		livereload().changed(file.path);
-	});
+  // Watch src paths and execute callback tasks as necessary
+  gulp.watch(paths.src.sass + '/**/*', ['css']);
+  gulp.watch(paths.src.images,         ['images']);
 });
 
-gulp.task('default', ['css', 'images']);
+/**
+ * Set up default task
+ */
+gulp.task('default', [
+  'images',
+  'css'
+]);
+
+/**
+ * Set up watch task
+ */
+gulp.task('watch', [
+  'default',
+  'watcher'
+]);
